17ac210b7ff11bf746ef3cabec0ae1dda9bcf717
Tam Mach <sayboras@yahoo.com>
2025-03-31 18:48:19 +1100

    fix: Add chmod with executable permission

--- lib/utils.bash
+++ lib/utils.bash
@@ -64,6 +64,9 @@ install_version() {
 		# TODO: Assert pwru executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+		echo "tool cmd: " $tool_cmd
+		echo "install path: " $install_path
+		chmod +x "$install_path/$tool_cmd"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
